17d07f88bfbf98a89db35214f1293707ba0ea721
Rubén Béjar <ruben>
2019-03-30 13:59:42 +0100

    A little cleaning and commenting.

--- gdtc/tests/insertHDFworkflow.py
+++ gdtc/tests/insertHDFworkflow.py
@@ -3,17 +3,18 @@ import gdtc.filters.file2file as f2f
 import gdtc.tasks.workflowbuilder as wfb
 
 
-# TODO: probar con un fichero HDF de verdad, que ahora falla por eso
+# TODO: probar con un fichero HDF de verdad, que no tenía uno a mano y peta
 if __name__ == '__main__':
+    # We create 2 filters
     f1 = f2f.hdf2tif(layer_num=0, dstSRS="EPSG:4358")
     _,coord_sys = str(f1.getParams()['dstSRS']).split(':')
     f2 = f2f.tif2sql(coord_sys = coord_sys, table = "table_dummy", db = "db_dummy")
+
+    # We chain them
     filterchain = f2f.FileFilterChain([f1,f2], "fichero_entrada", "fichero_salida", params={})
+    # We must simply run them in order
     filterchain.run()
 
-
+    # We may create and run a luigi workflow instead
     workflow = wfb.filter_chain_2_task_chain(filterchain)
-    #t1 = wfb.createFileTransformationTask(f1.getInputPath(), f1.getOutputPath(), f1.run)
-    #t2 = wfb.createFileTransformationTask(f1.getOutputPath(), "salida.sql", f2.run)
-    #workflow = wfb.createSequence(ts=[t1, t2])
     luigi.build([workflow])
