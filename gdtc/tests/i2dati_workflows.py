6bdb8aeaea9cdc2f756ac369cca74c22fc8d7363
Rubén Béjar <ruben>
2019-04-10 13:55:56 +0200

    Primeras pruebas

--- gdtc/tests/i2dati_workflows.py
+++ gdtc/tests/i2dati_workflows.py
@@ -8,12 +8,11 @@ class I2DATIWorkflowsTests(unittest.TestCase):
     def test_population(self):
         params = gdtcdb.add_output_db_params({}, 'localhost', 5432, user='postgres', password='mysecretpassword',
                                              db='posgres')
-
-        f1 = SHPtoDB(params={})
+        params['output_db_table'] = gdtcdb.get_random_table_name()
+        f1 = SHPtoDB(params)
         f1.set_input("""
         /home/rbejar/Nextcloud/Research_Notebook/Spatial_Information_Infrastructures_And_Geo_Data
         /Data/recintos_municipales_inspire_peninbal_etrs89/recintos_municipales_inspire_peninbal_etrs89.shp""")
-        f1.set_output()
 
         # latlon_etrs_89 = gdtcsrs.Srs('EPSG:4258') # Iberian Peninsula and Balearic Islands
         # latlon_wgs_84 = gdtcsrs.Srs('EPSG:4326') # Canary Islands. For proj.4 this is equivalent to the 4258, :-/
