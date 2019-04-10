e4e537f7918e9d6a9b3921ca0cf9e13973cbd964
Rubén Béjar <ruben>
2019-04-10 15:42:33 +0200

    Some tests already working

--- gdtc/tests/i2dati_workflows.py
+++ gdtc/tests/i2dati_workflows.py
@@ -5,16 +5,22 @@ import gdtc.aux.db as gdtcdb
 import tasks.workflowbuilder as wfb
 
 class I2DATIWorkflowsTests(unittest.TestCase):
-    def test_population(self):
-        params = gdtcdb.add_output_db_params({}, 'localhost', 5432, user='postgres', password='mysecretpassword',
-                                             db='posgres')
-        params['output_db_table'] = gdtcdb.get_random_table_name()
-        f1 = SHPtoDB(params)
-        f1.set_input("""
-        /home/rbejar/Nextcloud/Research_Notebook/Spatial_Information_Infrastructures_And_Geo_Data
-        /Data/recintos_municipales_inspire_peninbal_etrs89/recintos_municipales_inspire_peninbal_etrs89.shp""")
+    def setUp(self):
+        self.params = gdtcdb.add_output_db_params({}, 'localhost', 5432, user='postgres', password='mysecretpassword',
+                                             db='postgres')
 
-        # latlon_etrs_89 = gdtcsrs.Srs('EPSG:4258') # Iberian Peninsula and Balearic Islands
-        # latlon_wgs_84 = gdtcsrs.Srs('EPSG:4326') # Canary Islands. For proj.4 this is equivalent to the 4258, :-/
+    def test_population(self):
+        self.params['output_db_table'] = gdtcdb.get_random_table_name()
+        f1 = SHPtoDB(self.params)
+        f1.set_input("/home/rbejar/Nextcloud/Research_Notebook/Spatial_Information_Infrastructures_And_Geo_Data/Data/"
+                     "recintos_municipales_inspire_peninbal_etrs89/recintos_municipales_inspire_peninbal_etrs89.shp")
 
         f1.run()
+
+    def test_roads(self):
+        # These are some 220k rows. This filter takes some 2 minutes to run in my computer.
+        self.params['output_db_table'] = gdtcdb.get_random_table_name()
+        f2 = SHPtoDB(self.params)
+        f2.set_input("/home/rbejar/Nextcloud/Research_Notebook/Spatial_Information_Infrastructures_And_Geo_Data/Data/"
+                     "RT_ZARAGOZA/RT_VIARIA/rt_tramo_vial.shp")
+        f2.run()
