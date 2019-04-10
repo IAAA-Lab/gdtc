02da5bc9bb5359edc652f208491cdc7a554ca9b2
Rubén Béjar <ruben>
2019-04-10 17:55:37 +0200

    CSV2DB done

--- gdtc/tests/i2dati_workflows.py
+++ gdtc/tests/i2dati_workflows.py
@@ -1,6 +1,6 @@
 import luigi
 import unittest
-from filters.file2db import SHPtoDB
+import filters.file2db as gdtcf2db
 import gdtc.aux.db as gdtcdb
 import tasks.workflowbuilder as wfb
 
@@ -20,7 +20,19 @@ class I2DATIWorkflowsTests(unittest.TestCase):
     def test_roads(self):
         # These are some 220k rows. This filter takes some 2 minutes to run in my computer.
         self.params['output_db_table'] = gdtcdb.get_random_table_name()
-        f2 = SHPtoDB(self.params)
+        f2 = gdtcf2db.SHP2DB(self.params)
         f2.set_input("/home/rbejar/Nextcloud/Research_Notebook/Spatial_Information_Infrastructures_And_Geo_Data/Data/"
                      "RT_ZARAGOZA/RT_VIARIA/rt_tramo_vial.shp")
         f2.run()
+
+    def test_ine_provs(self):
+        self.params['output_db_table'] = gdtcdb.get_random_table_name()
+        f3 = gdtcf2db.CSV2DB(self.params)
+        f3.set_input("/home/rbejar/Nextcloud/Research_Notebook/Spatial_Information_Infrastructures_And_Geo_Data/Data/"
+                     "INE_Provs_2018.csv")
+        f3.run()
+
+        # We can pass pandas.to_csv parameters (this is a silly test to check that it does not fail)
+        f3.run(names=['col1, col2'])
+        # This is unnecessary, but it also works
+        f3.run(encoding='utf-8')
